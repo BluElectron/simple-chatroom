9ee308d7f8e7bc5b4e20a03e141ca9a6c830bbe8
BluElectron <ahartcyber@gmail.com>
2018-10-31 14:23:10 -0400

    Update UDP_CHAT_CLIENT.py

--- UDP_CHAT_CLIENT.py
+++ UDP_CHAT_CLIENT.py
@@ -1,9 +1,14 @@
+# Alison Hart
+# UDP Chat Client
+# Connects to local (or network) server IP for chatting purposes
+
+
 from socket import *
 serverName = '127.0.0.1'
 serverPort = 12000
 
 
-
+# initialize socket
 clientSocket = socket(AF_INET, SOCK_DGRAM)
 
 clientSocket.sendto(("JOIN").encode(), (serverName, serverPort))
@@ -11,7 +16,12 @@ message, serverAddress = clientSocket.recvfrom(2048)
 print(message.decode())
 
 response = ""
-while(response!="0"):
+
+
+while(response!="0"):       # While user wants to continue:
+    
+    # Menu
+    
     print("\n**************************************")
     print("*                MENU                  *")
     print("* 0. QUIT                              *")
@@ -21,22 +31,24 @@ while(response!="0"):
 
     response = input()
 
-    if (response =="1"):
+    
+    if (response =="1"):    # SEND MESSAGE
         chat = input("Enter message>>>")
         chat = "WRITE__"+chat
         clientSocket.sendto((chat).encode(), (serverName, serverPort))
         message, serverAddress = clientSocket.recvfrom(2048)
 
-    elif (response == "2"):
+    elif (response == "2"):  # GET CHAT LOG
         clientSocket.sendto(("GET").encode(), (serverName, serverPort))
         message, serverAddress = clientSocket.recvfrom(2048)
         message = message[6:]
         print(message.decode())
-    elif (response=="0"):
+        
+    elif (response=="0"):   # QUIT
         print("Goodbye, thanks for chatting!")
 
-    else:
+    else:                   # INVALID CHOICE
         print("You entered an invalid option. Try again!")
 
-clientSocket.close()
+clientSocket.close()        # Close connection
 
